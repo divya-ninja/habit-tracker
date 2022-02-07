11898f1e7ba414ba31fb1016c27042d0c4f9951d
divya-ninja <divya140294@gmail.com>
2022-02-07 23:17:19 +0530

    updated WeekView component

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -6,19 +6,33 @@ import { v4 as uuid } from 'uuid';
 class WeekView extends Component{
     constructor(props){
         super(props);
-        // Getting the state of the habits from the store and setting the initial state with it
+    
         this.state = {
-            currentHabits: store.getState()
+          habits: []
         }
-        
-        // function binding
+
         this.rerenderWeekView = this.rerenderWeekView.bind(this);
+    }
+    
+    // Getting the state of the habits from the local storage and setting the initial state with it
+    componentDidMount(){
+        let state = localStorage.getItem('habits');
+        
+        if(state === null){
+            localStorage.setItem('habits', JSON.stringify(new Array(0)));
+        
+        }
+        if(state !== null && state !== []){
+            this.setState({
+                habits: JSON.parse(state)
+            });
+        }
     }
 
     // rerendering the component whenever the state of a child component changes
     rerenderWeekView(){
         this.setState({
-            currentHabits: store.getState()
+            habits: store.getState()
         });
     }
 
@@ -31,7 +45,7 @@ class WeekView extends Component{
                 </div>
                 <ul style={{listStyleType: "none", marginBlockStart: "0px", paddingInlineStart: "0px"}}>
                     {/* Mapping over the list of habits and creating a week component for each habit */}
-                    {this.state.currentHabits.map(habit => (
+                    {this.state.habits.map(habit => (
                         <Week
                             description={habit.description} 
                             key={uuid()}
